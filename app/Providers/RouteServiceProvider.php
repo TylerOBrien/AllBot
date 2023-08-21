a0c6e478bc3197295b029d66c599b851a1e250dd
Tyler O'Brien <hello@tylerobrien.com>
2023-08-21 14:54:23 -0400

    Update RouteServiceProvider to use Croftvel

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -2,31 +2,72 @@
 
 namespace App\Providers;
 
+use App\Guards\Api\v1\ApiGuard;
+
 use Illuminate\Cache\RateLimiting\Limit;
 use Illuminate\Foundation\Support\Providers\RouteServiceProvider as ServiceProvider;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\RateLimiter;
-use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\{ RateLimiter, Route };
 
 class RouteServiceProvider extends ServiceProvider
 {
     /**
-     * The path to the "home" route for your application.
-     *
-     * Typically, users are redirected here after authentication.
+     * This const is expected as part of the Laravel framework but is not
+     * actually used.
      *
      * @var string
      */
-    public const HOME = '/home';
+    public const HOME = '/';
 
     /**
-     * Define your route model bindings, pattern filters, and other route configuration.
+     * Define your route model bindings, pattern filters, and other route
+     * configuration.
      *
      * @return void
      */
-    public function boot()
+    public function boot(): void
     {
         $this->configureRateLimiting();
+        $this->bindLiteralMeRouteParam();
+        $this->mapApiRoutes();
+    }
+
+    /**
+     * Implicitly convert all {user} params of 'me' to the currently
+     * authenticated user.
+     *
+     * @return void
+     */
+    protected function bindLiteralMeRouteParam(): void
+    {
+        Route::bind('user', function ($id) {
+            return $id === 'me' ? auth()->user() : $id;
+        });
+    }
+
+    /**
+     * Define the api routes for the application.
+     *
+     * These routes are typically stateless.
+     *
+     * @return void
+     */
+    protected function mapApiRoutes(): void
+    {
+        Route::group(
+            [
+                'prefix' => config('api.version'),
+                'domain' => config('api.domains.' . config('api.version')),
+            ],
+            function () {
+                $hasToken = ApiGuard::get()->hasToken();
+                $middlware = 'api.' . ($hasToken ? 'authenticated' : 'guest');
+                $path = 'routes/Api/' . config('api.version') .'/api.php';
+
+                Route::middleware($middlware)
+                     ->group(base_path($path));
+            },
+        );
     }
 
     /**
@@ -34,7 +75,7 @@ class RouteServiceProvider extends ServiceProvider
      *
      * @return void
      */
-    protected function configureRateLimiting()
+    protected function configureRateLimiting(): void
     {
         RateLimiter::for('api', function (Request $request) {
             return Limit::perMinute(60)->by($request->user()?->id ?: $request->ip());
