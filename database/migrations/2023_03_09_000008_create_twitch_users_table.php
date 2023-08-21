707dcefcb52d5edbc84720f4d238f04f84f827ca
Tyler O'Brien <hello@tylerobrien.com>
2023-08-21 12:47:20 -0400

    Add create twitch_users table migration

---
+++ database/migrations/2023_03_09_000008_create_twitch_users_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('twitch_users', function (Blueprint $table) {
+            $table->id();
+            $table->string('twitch_id');
+            $table->unsignedBigInteger('identity_id');
+            $table->string('email')->nullable();
+            $table->string('name')->nullable();
+            $table->string('profile_image_url')->nullable();
+            $table->timestamps();
+
+            $table->foreign('identity_id')
+                  ->references('id')->on('identities')
+                  ->onDelete('cascade')
+                  ->onUpdate('restrict');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('twitch_users');
+    }
+};
